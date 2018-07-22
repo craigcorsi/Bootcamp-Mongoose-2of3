e45780680040cc789257608b17d86c85e656af82
Craig <corsi007@umn.edu>
2018-07-22 16:28:41 -0500

    post routes

--- server.js
+++ server.js
@@ -94,7 +94,16 @@ app.get("/articles/:id", function (req, res) {
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
-  // ???
+  db.Note.create(req.body)
+  .then(function (dbNote) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+  })
+  .then(function (dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function (err) {
+    res.json(err);
+  });
 });
 
 // Start the server
